011f27e132dcf3526f433eb6d6a237a6c22b75cd
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2026-05-14 12:02:21 +0900

    헤더, 푸터 퍼블리싱

--- script/script.js
+++ script/script.js
@@ -1,7 +1,41 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    
+    asideMenuHandler();
 })
 
 
+const asideMenuHandler = () => {
+
+    //슬라이드 메뉴 열기
+    $('header .btn-wrap .menu-btn').click(function(){
+        $('.aside-menu').addClass('active');
+        $('.aside-menu .aside-content').addClass('active');
+    })
+
+    //슬라이드 메뉴 닫기
+    $('.aside-menu .btn-wrap .close-btn').click(function(){
+        $('.aside-menu').removeClass('active');
+        $('.aside-menu .aside-content').removeClass('active');
+    })
+    $(".aside-menu").click(function(){
+        $('.aside-menu').removeClass('active');
+        $('.aside-menu .aside-content').removeClass('active');
+    })
+    $(".aside-menu .aside-content").click(function(e){
+        e.stopPropagation();
+    })
+    $(".aside-menu .aside-content .sub-menu-list").click(function(e){
+        e.stopPropagation();
+    })
+    $(".aside-menu .aside-content .sub-menu-list .sub-item").click(function(e){
+        e.stopPropagation();
+    })
+
+    //서브 메뉴 토글
+    $('.aside-menu .menu-box-wrap .meu-list .item').click(function(){
+        $(this).toggleClass('show');
+        $(this).siblings('.sub-menu-list').slideToggle(300);
+    })
+}
